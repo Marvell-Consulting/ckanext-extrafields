ebbf0335008f21dd76b3ad9df73f95097af4cb18
Ralph Cowling <ralph@ralph-cowling.com>
2021-11-16 15:15:11 +0100

    make the plugin a simple holding pen for schemas

--- ckanext/extrafields/plugin.py
+++ ckanext/extrafields/plugin.py
@@ -15,102 +15,105 @@ statuses()
 
 
 class ExtrafieldsPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
-    plugins.implements(plugins.IDatasetForm)
-    plugins.implements(plugins.ITemplateHelpers)
-    plugins.implements(plugins.IConfigurer)
+    True
 
-    # IConfigurer
-    # This interface allows to implement a function update_config()
-    # that allows us to update the CKAN config, in our case we want to
-    # add an additional location for CKAN to look for templates.
+# class ExtrafieldsPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
+#     plugins.implements(plugins.IDatasetForm)
+#     plugins.implements(plugins.ITemplateHelpers)
+#     plugins.implements(plugins.IConfigurer)
 
-    def update_config(self, config):
-        # toolkit.add_public_directory(config_, 'public')
-        # toolkit.add_resource('fanstatic',
-        #                      'extrafields')
-        toolkit.add_template_directory(config, "templates")
+#     # IConfigurer
+#     # This interface allows to implement a function update_config()
+#     # that allows us to update the CKAN config, in our case we want to
+#     # add an additional location for CKAN to look for templates.
 
-    # IDatasetForm
-    # TODO: this can probably all go in favour of schema flow
-    def setup_template_variables(self, context, data_dict=None):
-        """
-        Adds variables to c just prior to the template being rendered that can
-        then be used within the form
-        """
-        c.resource_columns = model.Resource.get_columns()
-        try:
-            c.status_tags = toolkit.get_action("tag_list")(
-                context, {"vocabulary_id": STATUS_VOCAB}
-            )
+#     def update_config(self, config):
+#         # toolkit.add_public_directory(config_, 'public')
+#         # toolkit.add_resource('fanstatic',
+#         #                      'extrafields')
+#         toolkit.add_template_directory(config, "templates")
 
-        except NotFound:
-            c.capability_tags = None
+#     # IDatasetForm
+#     # TODO: this can probably all go in favour of schema flow
+#     def setup_template_variables(self, context, data_dict=None):
+#         """
+#         Adds variables to c just prior to the template being rendered that can
+#         then be used within the form
+#         """
+#         c.resource_columns = model.Resource.get_columns()
+#         try:
+#             c.status_tags = toolkit.get_action("tag_list")(
+#                 context, {"vocabulary_id": STATUS_VOCAB}
+#             )
 
-    def _modify_package_schema(self, schema):
-        # Add our custom_test metadata field to the schema, this one will use
-        # convert_to_extras instead of convert_to_tags.
-        schema.update(
-            {
-                "capabilities": [
-                    toolkit.get_validator("ignore_missing"),
-                    toolkit.get_converter("convert_to_tags")(CAPABILITY_VOCAB),
-                ],
-                "standard_status": [
-                    toolkit.get_validator("ignore_missing"),
-                    toolkit.get_converter("convert_to_tags")(STATUS_VOCAB),
-                ],
-            }
-        )
+#         except NotFound:
+#             c.capability_tags = None
 
-        return schema
+#     def _modify_package_schema(self, schema):
+#         # Add our custom_test metadata field to the schema, this one will use
+#         # convert_to_extras instead of convert_to_tags.
+#         schema.update(
+#             {
+#                 "capabilities": [
+#                     toolkit.get_validator("ignore_missing"),
+#                     toolkit.get_converter("convert_to_tags")(CAPABILITY_VOCAB),
+#                 ],
+#                 "standard_status": [
+#                     toolkit.get_validator("ignore_missing"),
+#                     toolkit.get_converter("convert_to_tags")(STATUS_VOCAB),
+#                 ],
+#             }
+#         )
 
-    def create_package_schema(self):
-        schema = super(ExtrafieldsPlugin, self).create_package_schema()
-        schema = self._modify_package_schema(schema)
-        return schema
+#         return schema
 
-    def update_package_schema(self):
-        schema = super(ExtrafieldsPlugin, self).update_package_schema()
-        schema = self._modify_package_schema(schema)
-        return schema
+#     def create_package_schema(self):
+#         schema = super(ExtrafieldsPlugin, self).create_package_schema()
+#         schema = self._modify_package_schema(schema)
+#         return schema
 
-    # get stuff from the database and return it in a manner
-    # that is useful for the form
-    def show_package_schema(self):
-        schema = super(ExtrafieldsPlugin, self).show_package_schema()
-        schema["tags"]["__extras"].append(toolkit.get_converter("free_tags_only"))
+#     def update_package_schema(self):
+#         schema = super(ExtrafieldsPlugin, self).update_package_schema()
+#         schema = self._modify_package_schema(schema)
+#         return schema
 
-        # get capabilities ID in order to filter tags
-        # capability_vocab_id = toolkit.get_action("vocabulary_show")(
-        #     {}, {"id": CAPABILITY_VOCAB}
-        # )["id"]
+#     # get stuff from the database and return it in a manner
+#     # that is useful for the form
+#     def show_package_schema(self):
+#         schema = super(ExtrafieldsPlugin, self).show_package_schema()
+#         schema["tags"]["__extras"].append(toolkit.get_converter("free_tags_only"))
 
-        schema.update(
-            {
-                "capabilities_selected": [
-                    # TODO: why does ignore_missing cut out selected capabilities?
-                    # toolkit.get_validator("ignore_missing"),
-                    toolkit.get_converter("convert_from_tags")(CAPABILITY_VOCAB),
-                ],
-                "current_status": [
-                    toolkit.get_converter("convert_from_tags")(STATUS_VOCAB),
-                ],
-            }
-        )
+#         # get capabilities ID in order to filter tags
+#         # capability_vocab_id = toolkit.get_action("vocabulary_show")(
+#         #     {}, {"id": CAPABILITY_VOCAB}
+#         # )["id"]
 
-        return schema
+#         schema.update(
+#             {
+#                 "capabilities_selected": [
+#                     # TODO: why does ignore_missing cut out selected capabilities?
+#                     # toolkit.get_validator("ignore_missing"),
+#                     toolkit.get_converter("convert_from_tags")(CAPABILITY_VOCAB),
+#                 ],
+#                 "current_status": [
+#                     toolkit.get_converter("convert_from_tags")(STATUS_VOCAB),
+#                 ],
+#             }
+#         )
 
-    def is_fallback(self):
-        # Return True to register this plugin as the default handler for
-        # package types not handled by any other IDatasetForm plugin.
-        return True
+#         return schema
 
-    def package_types(self):
-        # This plugin doesn't handle any special package types, it just
-        # registers itself as the default (above).
-        return []
+#     def is_fallback(self):
+#         # Return True to register this plugin as the default handler for
+#         # package types not handled by any other IDatasetForm plugin.
+#         return True
 
-    # ITemplateHelpers
+#     def package_types(self):
+#         # This plugin doesn't handle any special package types, it just
+#         # registers itself as the default (above).
+#         return []
 
-    def get_helpers(self):
-        return {"capabilities": capabilities, "statuses": statuses}
+#     # ITemplateHelpers
+
+#     def get_helpers(self):
+#         return {"capabilities": capabilities, "statuses": statuses}
